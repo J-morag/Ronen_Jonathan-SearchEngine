7ee9064b1767f768d7137b29cfc639c4774dd6aa
J-morag <38430800+J-morag@users.noreply.github.com>
2018-10-31 16:02:53 +0200

    Initial commit

---
+++ src/Indexing/ReadFile.java
@@ -0,0 +1,5 @@
+package Indexing;
+
+public class ReadFile {
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        createIndex();
+    }
+
+    private static void createIndex() {
+    }
+}
