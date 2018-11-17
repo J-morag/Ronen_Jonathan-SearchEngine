2b8c2643d3ae5ec5bdd70568474fb1f4233e537b
J-morag <38430800+J-morag@users.noreply.github.com>
2018-11-17 17:08:25 +0200

    removed possible framework for indexing

--- src/main/java/Indexing/Index/PermanentPosting.java
+++
@@ -1,7 +0,0 @@
-package Indexing.Index;
-/**
- * represents Posting for a term.
- * The main difference from TemporaryPosting is that it only holds a reference to the location of the Posting in secondary memory.
- */
-public class PermanentPosting extends APosting {
-}
--- src/main/java/Indexing/Index/TemporaryPosting.java
+++
@@ -1,9 +0,0 @@
-package Indexing.Index;
-
-/**
- * represents an index entry.
- * The main difference from PermanentPosting is that it holds the information  in main memory, and is meant to be
- * written onto disk during the creation of the index.
- */
-public class TemporaryPosting extends APosting {
-}
