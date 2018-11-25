f85f28b5474ca83879fc1d933805730020fa06b7
J-morag <38430800+J-morag@users.noreply.github.com>
2018-11-25 09:36:04 +0200

    added shortOnlyPostingOutputStream

--- src/test/java/Indexing/Index/IO/BasicPostingOutputStreamTest.java
+++ src/test/java/Indexing/Index/IO/BasicPostingOutputStreamTest.java
@@ -93,14 +93,14 @@ class BasicPostingOutputStreamTest {
 
     @Test
     void testTime() throws IOException {
-        Posting[] postings = new Posting[50];
+        Posting[] postingsForOneTerm = new Posting[100];
         Posting p1 = new Posting("dco01", (short)13, (short)78, (short)1900, "beer sheva", "hebrew", true, false);
         Posting p2 = new Posting("dco22", (short)2349, (short)0, (short)1910, "York", "English", false, false);
-        for (int i = 0; i <25 ; i++) {
-            postings[i] = p1;
+        for (int i = 0; i <50 ; i++) {
+            postingsForOneTerm[i] = p1;
         }
-        for (int i = 25; i <50 ; i++) {
-            postings[i] = p2;
+        for (int i = 50; i <postingsForOneTerm.length ; i++) {
+            postingsForOneTerm[i] = p2;
         }
 
         clean();
@@ -108,13 +108,13 @@ class BasicPostingOutputStreamTest {
         long startTime = System.currentTimeMillis();
 
         for (int j = 0; j <100000 ; j++) {
-            out.write(postings);
+            out.write(postingsForOneTerm);
         }
 
         long time = (System.currentTimeMillis() - startTime);
 
-        System.out.println("time for 100,000 terms with 50 postings each (S): " + (time)/1000);
-        System.out.println("time for 100,000 terms with 50 postings each, fifty times (m): " + (time)*50/1000/60);
+        System.out.println("time for 100,000 terms with 100 postings each (ms): " + (time));
+        System.out.println("time for 100,000 terms with 100 postings each, fifty times (m): " + (time)*50/1000/60);
 
     }
 }
