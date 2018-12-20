e3ec1f5d285e95133b4eca2565bc9b72c15ffba4
J-morag <38430800+J-morag@users.noreply.github.com>
2018-12-20 15:24:02 +0200

    PartB. Added Querying package and associated classes

---
+++ src/main/java/Querying/WeightSet.java
@@ -0,0 +1,20 @@
+package Querying;
+
+/**
+ * A set of weights for the {@link Ranker Ranker} class.
+ * Is used to give different weights to the various factors considered when ranking a document's relevancy to a query.
+ */
+public class WeightSet {
+
+    final int titleWeight;
+    final int beginningWeight;
+    final int frequencyWeight;
+    final int exactTermMatchWeight;
+
+    public WeightSet(int titleWeight, int beginningWeight, int frequencyWeight, int exactTermMatchWeight) {
+        this.titleWeight = titleWeight;
+        this.beginningWeight = beginningWeight;
+        this.frequencyWeight = frequencyWeight;
+        this.exactTermMatchWeight = exactTermMatchWeight;
+    }
+}
